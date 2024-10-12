990cd7d92f23a73afe52e33c2c79a2fdfc8818f8
RichardSantosPaiva <richardpaiva0101@gmail.com>
2024-10-12 20:32:37 -0300

    alterando

---
+++ app/index.js
@@ -0,0 +1,80 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text , StyleSheet,TouchableOpacity} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Camera from './camera/'
+import Location from './location/'
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={{color:'white', fontSize:50}}>Home </Text>
+      <Text style={{color:'#a7b8c2', fontSize:18,marginTop:10}}>Bem-vindo ao Kaiduchard</Text>
+      <TouchableOpacity
+        style={styles.button}
+        title="Camera"
+        onPress={() => navigation.navigate('Camera')}
+      >
+          <Text style={styles.buttonText}>Camera</Text>
+      </TouchableOpacity>
+       
+       <TouchableOpacity
+        style={styles.button}
+        title="Location"
+        onPress={() => navigation.navigate('Location')}
+      >
+        <Text style={styles.buttonText}>Location</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Camera" component={Camera} />
+        <Stack.Screen name="Location" component={Location} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop:80, 
+    alignItems: 'center',
+     backgroundColor:'#08082e',
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  button:{
+    width:200,
+    alignItems:'center',
+    borderColor:'#fcbf05',
+    borderWidth: 3,
+    borderRadius: 8, // Add rounded corners to the button
+    padding:10,
+    margin: 30, // Add margin for spacing between buttons
+    backgroundColor:'#08082e',
+  },
+  buttonText: {
+    color: '#fcbf05', // Text color for the button
+    fontSize: 20, // Font size for the button text
+  },
+  map: {
+    flex: 1,
+    width: '100%',
+  },
+});
---
+++ app/location/index.js
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, ActivityIndicator } from 'react-native';
+import * as Location from 'expo-location';
+import MapView, { Marker } from 'react-native-maps';
+
+export default function LocationPage() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const requestLocationPermission = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permissão para acessar a localização foi negada');
+        setLoading(false);
+        return;
+      }
+
+      try {
+        let currentLocation = await Location.getCurrentPositionAsync({});
+        setLocation(currentLocation);
+      } catch (error) {
+        setErrorMsg('Erro ao obter a localização: ' + error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    requestLocationPermission();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {loading ? (
+        <ActivityIndicator size="large" color="#ffffff" />
+      ) : errorMsg ? (
+        <Text style={styles.error}>{errorMsg}</Text>
+      ) : location ? (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+          />
+        </MapView>
+      ) : null}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#08082e',
+  },
+  map: {
+    borderRadius: 10,
+    flex: 1,
+    width: '100%',
+  },
+  error: {
+    color: 'red',
+  },
+});
